d756cfbe06ec485935c5075af197c1cb0fdd50bf
Samadsust71 <samadsust71@gmail.com>
2025-07-01 23:42:10 +0600

    implemented dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import { toast } from "sonner"
-import { Button } from "./components/ui/button"
+import { Moon, Sun } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useTheme } from "./providers/theme-provider"
 
 
-
-function App() {
- 
+ const App = () =>{
+  const { setTheme } = useTheme()
 
   return (
     <>
-      <Button variant={"destructive"}>Click me</Button>
-       <Button
-      variant="outline"
-      onClick={() =>
-        toast("Event has been created")
-      }
-    >
-      Show Toast
-    </Button>
-      
+    <div className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <h1>Logo</h1>
+      <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+      </nav>
+    </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from './components/ui/sonner.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster  position='top-center'/>
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster position="top-center" />
+    </ThemeProvider>
+  </StrictMode>
+);
